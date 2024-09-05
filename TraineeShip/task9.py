0de0ed57deb31d9f12f313c1dfe7cbafcc1fd719
Aayush <manandharaayush61@gmail.com>
2024-09-05 15:40:32 +0545

    completed task9 of python

--- TraineeShip/task9.py
+++ TraineeShip/task9.py
@@ -20,9 +20,37 @@ def addContact():
          print("Contact added Successfully!")
          
 def editContact():
-    print('Edit an existing contact')
     print("Enter name to update contact")
-    contact = input()
+    userInput = input()
+    
+    contactFound = False
+    updatedContacts = []
+    
+    with open(contactBook,"r") as file:
+        contents = file.read()
+        lines = contents.split("\n")
+                
+        for contact in lines:  
+            if userInput.lower() in contact.lower():  
+                print("Contact exists:", contact)
+                
+                print("Edit the contact (name - number):", end=" ")
+                updated = input()
+                updatedContacts.append(f"{updated} \n")  # Add the updated contact to the list
+                contactFound = True
+            else:
+                updatedContacts.append(f"{contact}\n")  
+        
+        if contactFound:
+            with open(contactBook, "w") as file:
+                file.writelines(updatedContacts)
+                print("Contact updated successfully!")
+        else:
+            print("No such contact found.")
+            
+                   
+        
+                
     
 def searchContact():
     print("Search an exsisting contact")
@@ -30,19 +58,18 @@ def searchContact():
     
     search = input()     
     
-    contactFound = False
-    
+    found = False
+        
     with open(contactBook, "r") as file:
         contents = file.read()
         
         lines = contents.lower().split("\n")
+        
         for names in lines:
-            print("Yo chai names hai:",names)
-            if(search.lower() in names):
+            if(search.lower() in names.lower()):
                 print("Contact Found")
                 print(names)
                 break
-    
     
     
     
@@ -53,3 +80,5 @@ if(command == "add"):
     addContact()
 if(command == "search"):
     searchContact()
+if(command == "edit"):
+    editContact()
